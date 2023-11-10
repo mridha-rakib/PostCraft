7a7bcf368b8e5483c26c76b20e53bbd196f3c27f
RAKIB <rakibmahmudmridha@gmail.com>
2023-11-10 16:23:14 +0600

    postCraft project setup

---
+++ config/dbConnect.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://rakibmahmudmridha:eRC2HG7yS7ibRoe0@cluster0.of7yscw.mongodb.net/postCraftDB"
+    );
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.log("DB connection error: ", error.message);
+  }
+};
+
+dbConnect();
+module.exports = dbConnect;
---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+// app initialize
+const app = express();
+
+// mongodb+srv://rakibmahmudmridha:<password>@cluster0.of7yscw.mongodb.net/
+// eRC2HG7yS7ibRoe0
